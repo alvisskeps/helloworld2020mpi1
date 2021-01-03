450bef77b56a6aecb2370ab2a87bfb031f8a30a2
alvis.skeps <alvis.skeps98@gmail.com>
2021-01-03 22:40:59 +0200

    Bug fixes, and students array update

--- app/src/main/java/lv/alvis/helloworld/MainActivity.java
+++ app/src/main/java/lv/alvis/helloworld/MainActivity.java
@@ -2,11 +2,9 @@ package lv.alvis.helloworld;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +14,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList selectedItems = new ArrayList();
-    String[] yourArray = getResources().getStringArray(R.array.students_array);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,25 +40,27 @@ public class MainActivity extends AppCompatActivity {
                         .setMultiChoiceItems(R.array.students_array, null,
                                 new DialogInterface.OnMultiChoiceClickListener() {
                                     @Override
-                                    public void onClick(DialogInterface dialog, int which,
-                                                        boolean isChecked) {
+                                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                        Resources res = getResources();
+                                        String[] studentsArray = res.getStringArray(R.array.students_array);
+
                                         if (isChecked) {
                                             selectedItems.add(which);
-                                            Toast.makeText(MainActivity.this, "student selected " + yourArray[which], Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(MainActivity.this, "student selected " + studentsArray[which], Toast.LENGTH_SHORT).show();
                                         } else if (selectedItems.contains(which)) {
                                             selectedItems.remove(Integer.valueOf(which));
-                                            Toast.makeText(MainActivity.this, "student removed " + yourArray[which], Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(MainActivity.this, "student removed " + studentsArray[which], Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 })
                         .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                // FIRE ZE MISSILES!
+                                Toast.makeText(MainActivity.this, "You clicked OK", Toast.LENGTH_SHORT).show();
                             }
                         })
                         .setNegativeButton("Close", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                // User cancelled the dialog
+                                Toast.makeText(MainActivity.this, "You closed dialog", Toast.LENGTH_SHORT).show();
                             }
                         });
 
